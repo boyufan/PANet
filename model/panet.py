eafbc37e97a850e6bb8a02637ddeb54bb2879638
boyufan <fbygg@126.com>
2021-12-09 09:10:56 +0200

    update

--- model/panet.py
+++ model/panet.py
@@ -31,24 +31,27 @@ import numpy as np
 #         x = x[:, :, :, ::-1]
 #     return x
 
+
 def cen_bias(input_shape):
-  mu = [input_shape[0]*0.5,input_shape[1]*0.5]
-  sigma = input_shape[0]*0.75
-  # mask
-  cen_bias = np.zeros((input_shape[0],input_shape[1],1))
-  for y1 in range(input_shape[0]):
-      for x1 in range(input_shape[1]):
-          exponent = (math.pow((y1+1-mu[0]),2) + math.pow((x1+1-mu[0]),2))/(2*sigma);
-          amplitude = 1 / (2 * math.sqrt(2*math.pi));  
-          cen_bias[y1,x1,:] = amplitude * math.exp(-exponent);
-  cen_bias_max = max(cen_bias.flatten())
-  cen_bias_min = min(cen_bias.flatten())
-  cen_bias = (cen_bias-cen_bias_min)/(cen_bias_max-cen_bias_min)
-  return cen_bias
+    mu = [input_shape[0]*0.5, input_shape[1]*0.5]
+    sigma = input_shape[0]*0.75
+    # mask
+    cen_bias = np.zeros((input_shape[0], input_shape[1], 1))
+    for y1 in range(input_shape[0]):
+        for x1 in range(input_shape[1]):
+            exponent = (math.pow((y1+1-mu[0]), 2) +
+                        math.pow((x1+1-mu[0]), 2))/(2*sigma)
+            amplitude = 1 / (2 * math.sqrt(2*math.pi))
+            cen_bias[y1, x1, :] = amplitude * math.exp(-exponent)
+    cen_bias_max = max(cen_bias.flatten())
+    cen_bias_min = min(cen_bias.flatten())
+    cen_bias = (cen_bias-cen_bias_min)/(cen_bias_max-cen_bias_min)
+    return cen_bias
+
 
 def PANET(input_shape, prior, mapping, p_vec, num_classes=81):
     if prior == []:
-      prior = cen_bias(input_shape)
+        prior = cen_bias(input_shape)
 
     upsample_factor = 15
     # voc_classes = ['Aeroplane', 'Bicycle', 'Bird', 'Boat', 'Bottle',
@@ -59,7 +62,7 @@ def PANET(input_shape, prior, mapping, p_vec, num_classes=81):
     # base_model = SSD300(input_shape, num_classes)
     # base_model.load_weights('weights_SSD300.hdf5', by_name=True)
     # pool5 = base_model.get_layer('pool5').output
-    net = {} 
+    net = {}
     # Block 1
     input_tensor = input_tensor = Input(shape=input_shape)
     img_size = (input_shape[1], input_shape[0])
@@ -131,22 +134,24 @@ def PANET(input_shape, prior, mapping, p_vec, num_classes=81):
     net['pool5'] = MaxPooling2D((3, 3), strides=(1, 1), border_mode='same',
                                 name='pool5')(net['conv5_3'])
 
+    # 以上是VGG-16的网络结构，2个64，2个128，3个256，3个512，3个512
+
     # ReadOut
     # ReadOut是什么？？？
     # 这里定义的和论文中的不一致，论文中式64，128，4，1，这里是16，32，2，1
-    net['ro1'] = Convolution2D(16, 1, 1, activation='relu', 
+    net['ro1'] = Convolution2D(16, 1, 1, activation='relu',
                                border_mode='same',
                                name='ro1')(net['pool5'])
-    net['ro2'] = Convolution2D(32, 1, 1, activation='relu', 
+    net['ro2'] = Convolution2D(32, 1, 1, activation='relu',
                                border_mode='same',
                                name='ro2')(net['ro1'])
-    net['ro3'] = Convolution2D(2, 1, 1, activation='relu', 
+    net['ro3'] = Convolution2D(2, 1, 1, activation='relu',
                                border_mode='same',
                                name='ro3')(net['ro2'])
-    net['ro4'] = Convolution2D(1, 1, 1, activation='relu', 
+    net['ro4'] = Convolution2D(1, 1, 1, activation='relu',
                                border_mode='same',
                                name='ro4')(net['ro3'])
-    # net['upsample_main'] = UpSampling2D(size=(upsample_factor, upsample_factor), 
+    # net['upsample_main'] = UpSampling2D(size=(upsample_factor, upsample_factor),
     #                       name='upsample_main')(net['ro4'])
     # # blur
     # net['main_blur'] = GaussianNoise(0.01, name='main_blur')(net['upsample_main'])
@@ -174,11 +179,12 @@ def PANET(input_shape, prior, mapping, p_vec, num_classes=81):
     net['conv7_1'] = Convolution2D(128, 1, 1, activation='relu',
                                    border_mode='same',
                                    name='conv7_1')(net['conv6_2'])
-    net['zero_padding2d_1'] = ZeroPadding2D(name="zero_padding2d_1")(net['conv7_1'])
+    net['zero_padding2d_1'] = ZeroPadding2D(
+        name="zero_padding2d_1")(net['conv7_1'])
     net['conv7_2'] = Convolution2D(256, 3, 3, subsample=(2, 2),
                                    activation='relu', border_mode='valid',
                                    name='conv7_2')(net['zero_padding2d_1'])
-    
+
     # Block 8
     net['conv8_1'] = Convolution2D(128, 1, 1, activation='relu',
                                    border_mode='same',
@@ -212,7 +218,6 @@ def PANET(input_shape, prior, mapping, p_vec, num_classes=81):
                         name='conv4_3_norm_mbox_priorbox')
     net['conv4_3_norm_mbox_priorbox'] = priorbox(net['conv4_3_norm'])
 
-
     # Prediction from fc7
     num_priors = 6
     net['fc7_mbox_loc'] = Convolution2D(num_priors * 4, 3, 3,
@@ -232,8 +237,6 @@ def PANET(input_shape, prior, mapping, p_vec, num_classes=81):
                         variances=[0.1, 0.1, 0.2, 0.2],
                         name='fc7_mbox_priorbox')
     net['fc7_mbox_priorbox'] = priorbox(net['fc7'])
-
-
 
     # Prediction from conv6_2
     num_priors = 6
@@ -255,7 +258,6 @@ def PANET(input_shape, prior, mapping, p_vec, num_classes=81):
                         name='conv6_2_mbox_priorbox')
     net['conv6_2_mbox_priorbox'] = priorbox(net['conv6_2'])
 
-
     # Prediction from conv7_2
     num_priors = 6
     x = Convolution2D(num_priors * 4, 3, 3, border_mode='same',
@@ -275,8 +277,6 @@ def PANET(input_shape, prior, mapping, p_vec, num_classes=81):
                         variances=[0.1, 0.1, 0.2, 0.2],
                         name='conv7_2_mbox_priorbox')
     net['conv7_2_mbox_priorbox'] = priorbox(net['conv7_2'])
-
-
 
     # Prediction from conv8_2
     num_priors = 6
@@ -298,7 +298,6 @@ def PANET(input_shape, prior, mapping, p_vec, num_classes=81):
                         name='conv8_2_mbox_priorbox')
     net['conv8_2_mbox_priorbox'] = priorbox(net['conv8_2'])
 
-
     # Prediction from pool6
     num_priors = 6
     x = Dense(num_priors * 4, name='pool6_mbox_loc_flat')(net['pool6'])
@@ -318,7 +317,6 @@ def PANET(input_shape, prior, mapping, p_vec, num_classes=81):
     net['pool6_reshaped'] = Reshape(target_shape,
                                     name='pool6_reshaped')(net['pool6'])
     net['pool6_mbox_priorbox'] = priorbox(net['pool6_reshaped'])
-
 
     # Gather all predictions
     net['mbox_loc'] = merge([net['conv4_3_norm_mbox_loc_flat'],
@@ -367,27 +365,30 @@ def PANET(input_shape, prior, mapping, p_vec, num_classes=81):
     # 在这一步引入pvec和mapping
     net['scat'] = Scat(scat2cat=mapping, pvec=p_vec)(net['nms'])
 
-    net['scat_conv1'] = Convolution2D(32, 1, 1, activation='relu', 
-                               border_mode='same',
-                               name='scat_conv1')(net['scat'])
-    net['scat_conv2'] = Convolution2D(2, 1, 1, activation='relu', 
-                               border_mode='same',
-                               name='scat_conv2')(net['scat_conv1']) 
-    net['scat_conv3'] = Convolution2D(1, 1, 1, activation='relu', 
-                               border_mode='same',
-                               name='scat_conv3')(net['scat_conv2'])                                                        
+    net['scat_conv1'] = Convolution2D(32, 1, 1, activation='relu',
+                                      border_mode='same',
+                                      name='scat_conv1')(net['scat'])
+    net['scat_conv2'] = Convolution2D(2, 1, 1, activation='relu',
+                                      border_mode='same',
+                                      name='scat_conv2')(net['scat_conv1'])
+    net['scat_conv3'] = Convolution2D(1, 1, 1, activation='relu',
+                                      border_mode='same',
+                                      name='scat_conv3')(net['scat_conv2'])
     # net['pref_blur'] = GaussianNoise(2, name='pref_blur')(net['scat'])
 
     # 上下两个网络结合
-    net['comb_1'] = merge([net['ro4'], net['scat_conv3']], mode='sum', name='comb_1')
+    net['comb_1'] = merge([net['ro4'], net['scat_conv3']],
+                          mode='sum', name='comb_1')
     # net['comb_1'] = merge([net['main_blur'], net['pref_blur']], mode='sum', name='comb_1')
     # 论文中的8和1在哪里？？
-    net['upsample_main'] = UpSampling2D(size=(upsample_factor, upsample_factor), 
-                          name='upsample_main')(net['comb_1'])
+    net['upsample_main'] = UpSampling2D(size=(upsample_factor, upsample_factor),
+                                        name='upsample_main')(net['comb_1'])
     # # blur
-    net['main_blur'] = GaussianNoise(0.01, name='main_blur')(net['upsample_main'])
+    net['main_blur'] = GaussianNoise(
+        0.01, name='main_blur')(net['upsample_main'])
 
-    net['comb_2'] = CentralBias(cen_bias=prior, name='comb_2')(net['main_blur'])
+    net['comb_2'] = CentralBias(
+        cen_bias=prior, name='comb_2')(net['main_blur'])
 
     net['result'] = Activation('sigmoid', name='result')(net['comb_2'])
 
